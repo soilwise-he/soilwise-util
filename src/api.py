61da0e6bbaf673cbd5b96ac7f6728b0906134f84
Paul van Genuchten <genuchten@yahoo.com>
2025-09-25 14:24:02 +0200

    update api

--- src/api.py
+++ src/api.py
@@ -4,10 +4,11 @@ from databases import Database
 from typing import List, Optional
 from pydantic import BaseModel
 from datetime import datetime
-import asyncpg
+import asyncpg, requests
 import logging
 import os
-from urllib.parse import quote_plus
+
+from urllib.parse import quote_plus, quote
 
 # Load environment variables from .env file
 load_dotenv()
@@ -58,15 +59,93 @@ async def fetch_data(query: str, values: dict = {}):
 
 # Endpoint to retrieve data with redirection statuses
 @app.get('/items', response_model=List[FeedResponse])
-async def get_items(offset: int = 0, limit: int = 10):
-    query = f"""
-        SELECT *
-        FROM {schema}.feeds 
-        ORDER by published desc
-        limit :limit offset :offset
-    """
-    data = await fetch_data(query=query,values={'limit':limit,'offset':offset})
+async def get_items(offset: int = 0, limit: int = 10, keywords: str = '', project: str = ''):
+    query = f"""SELECT * FROM {schema}.feeds f left join {schema}.projects p on f.project = p.code where true """ # join on code because it is always populated
+    if keywords != '':
+        query += "and replace(lower(f.tags), ' ','') like :kw "  
+    if project != '':
+        query += 'and (p.grantnr = :prj or p.code = :prj)' 
+    query += """ORDER by published desc limit :limit offset :offset"""
+    data = await fetch_data(query=query,values={'limit':limit,'offset':offset,'kw':f'%{keywords}%','prj':project})
     return data
+
+# Endpoint to get status of a doi
+@app.get('/status/{item:path}', response_model=List[str])
+async def status(item):
+    resp = [f"Test item {quote(item)}"]
+    if item:
+        
+        # is id in soilwise?
+        qry1 = """SELECT identifier from public.records WHERE identifier like :item or identifier like :splititem"""
+        d1 = await fetch_data(query=qry1, values={'item': '%'+item, 'splititem': '%'+(item.split('/').pop() or 'random-non-matching-string') })
+        qry2 = """SELECT identifier,error from harvest.items WHERE identifier like :item or identifier like :splititem"""
+        d2 = await fetch_data(query=qry2, values={'item': item, 'splititem': '%'+(item.split('/').pop() or 'random-non-matching-string')})
+        if len(d1) > 0:
+            resp.append(f"Record {d1[0][0]} exists in Soilwise")
+        elif len(d2) > 0:
+            resp.append(f"Record {d2[0][0]} was harvested but did not make it to the final catalogue; {d2[0][1]}")
+        else:
+            resp.append("Record not available in Soilwise")    
+            
+            # is id in openaire?
+            req = f"https://api.openaire.eu/search/researchProducts?format=json&doi={item.split('doi.org/').pop()}"
+            try:
+                res = requests.get(req,headers={'accept':'application/json'})
+                res2 = res.json()
+                if res2:
+                    res3 = res2.get('response',{}).get('results')
+                    if res3: 
+                        rels2 = res3.get('result',[{}])[0].get('metadata',{}).get('oaf:entity',{}).get('oaf:result',{}).get('rels',{})
+                        if rels2:
+                            rels = rels2.get('rel',[{}])
+                            code = ''
+                            acronym = ''
+                            if isinstance(rels, dict):
+                                rels = [rels] 
+                            for r in rels:
+                                print('r', r)
+                                if isinstance(r, dict) and r.get('funding',{}).get('funder',{}).get('@name') == 'European Commission':
+                                    code = r.get('code',{}).get('$','')
+                                    acronym = r.get('acronym',{}).get('$','')
+                            if code == '':
+                                resp.append("Record is in OpenAire, but no relevant HE grantnr has been found")
+                            else:
+                                # check if grantnr is in list
+                                qry3 = """SELECT grantnr, code FROM harvest.projects WHERE grantnr = :item"""
+                                d3 = await fetch_data(query=qry3, values={'item': code})
+                                if len (d3) > 0:
+                                    resp.append(f"Record is included in OpenAire, and is related to project {code}:{acronym}, that project is tagged by ESDAC as being soil related")
+                                else:    
+                                    resp.append(f"Record is included in OpenAire, and is related to project {code}:{acronym}, however that project is not tagged by ESDAC as being soil related")
+                        else:
+                            resp.append(f"Record has no funding relations") 
+                    else:
+                        resp.append(f"Record is not in OpenAire")
+                        # see if in Datacite
+                        req2 = f"https://doi.org/{item.split('doi.org/').pop()}"
+                        try:
+                            res2 = requests.get(req2,headers={'accept':'application/x-bibtex'}).text()
+                            print(res2)
+                        except Exception as e:
+                            resp.append(f"Error query bibtex, {e}") 
+                else:
+                    resp.append(f"No OpenAire result") 
+                
+            except Exception as e: 
+                resp.append(f"Error while querying OpenAire, {e}")
+        
+    return resp
+
+    # is id in data.europa.eu?  
+
+    # quri = f"https://data.europa.eu/api/hub/search/datasets/{}"
+    #try:
+    #            d = requests.get(req,headers={'accept':'application/json'}).json()
+
+        # does metadata include keyword soil?
+    # is id in cordis?
+
+
 
 # Start the application
 @app.on_event('startup')
